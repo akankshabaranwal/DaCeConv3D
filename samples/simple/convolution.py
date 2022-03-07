0702592e65093c57db1db5c08afeeaf6739caab9
abaranwal <akanksha.baranwal@inf.ethz.ch>
2022-03-07 23:10:41 +0100

    added input depth

--- samples/simple/convolution.py
+++ samples/simple/convolution.py
@@ -1,45 +1,6 @@
 # Copyright 2019-2021 ETH Zurich and the DaCe authors. All rights reserved.
 
 # Convolution sample code
-# An image in(x,y) with a kernel kernel(x,y).
-#
-# Produce a new image out(x,y)
-'''
-kCenterX = kCols / 2;
-kCenterY = kRows / 2;
-for (i = 0; i < rows; ++i) // rows  {
-    for (j = 0; j < cols; ++j) //columns    {
-        for (m = 0; m < kRows; ++m)        {
-            mm = kRows - 1 - m; // row index of flipped kernel
-            for ( n = 0; n < kCols; ++n )            {
-                nn = kCols - 1 - n;
-                ii = i + (kCenterY - mm);
-                jj = j + (kCenterX - nn);
-                if ( ii >= 0 && ii < rows && jj >=0 && jj < cols )
-                    out[i][j] += in[ii][jj] * kernel[mm][nn];
-            }
-        }
-    }
-}
-'''
-
-# //CPU function: conv_img_cpu
-# //Parameters: float *img, float *kernel, float *imgf, int Nx, int Ny, int kernel_size
-# //center: center of kernel
-#   for (int i = center; i<(Ny-center); i++)
-#     for (int j = center; j<(Nx-center); j++){
-#         //Convolution Operator:
-#         sum = 0;
-#         for (int ki = 0; ki<kernel_size; ki++)
-#            for (int kj = 0; kj<kernel_size; kj++){
-#                ii = j + kj - center;
-#                jj = i + ki - center;
-#                sum+=img[jj*Nx+ii]*kernel[ki*kernel_size + kj];
-#                }
-#         imgf[i*Nx +j] = sum;
-#         }
-#
-
 
 import click
 import dace
@@ -53,6 +14,7 @@ import dace.libraries.blas
 # Define symbolic sizes for arbitrary inputs
 rows = dace.symbol('rows')
 cols = dace.symbol('cols')
+indepth = dace.symbol('indepth')
 w = dace.symbol('w')
 
 # Define data type to use
@@ -61,24 +23,27 @@ np_dtype = np.float64
 
 # Simple unoptimized convolution code
 @dace.program
-def convolution2D(Input: dtype[rows, cols], kernel: dtype[w, w], Output: dtype[rows, cols]):
-    tmp = np.zeros([rows, cols, w*w], dtype = Input.dtype)
-    for i,j,m,n in dace.map[w/2:rows-w/2, w/2:cols-w/2, 0:w, 0:w]:
+def convolution3D(Input: dtype[rows, cols, indepth], kernel: dtype[w, w, indepth], Output: dtype[rows, cols]):
+    tmp = np.zeros([rows, cols, indepth*w*w], dtype = Input.dtype)
+    for i,j,d,m,n in dace.map[w/2:rows-w/2, w/2:cols-w/2,0:indepth, 0:w, 0:w]:
         with dace.tasklet:
-            in_A << Input[i - w/2 + m, j - w/2 + n]
-            in_B << kernel[ w-1-m, w-1-n]
-            out >> tmp[i, j, m*w+n]
+            in_A << Input[i - w/2 + m, j - w/2 + n,d]
+            in_B << kernel[ w-1-m, w-1-n,d]
+            out >> tmp[i, j, (d*(w*w)) + (m*w)+n]
 
             out = in_A * in_B
 
     dace.reduce(lambda a,b:a+b, tmp, Output, axis=2, identity=0)
 
+
 # Normal code for reference
 def refconvolution2D(Input, kernel):
-    RefOutput = np.zeros_like(Input)
+
     Refw = kernel.shape[0]
     Refrows = Input.shape[0]
     Refcols = Input.shape[1]
+    Refindepth = Input.shape[2]
+    RefOutput = np.zeros((Refrows, Refcols), dtype=np_dtype)
     Reftmpii = np.int_(Refw/2)
     Reftmpjj = np.int_(Refw/2)
     RefwCenter = np.int_(Refw / 2)
@@ -91,9 +56,10 @@ def refconvolution2D(Input, kernel):
                 Refii = i - Reftmpii + m
                 Refmm = Refw - 1 - m
                 for n in range(0,Refw):
-                    Refjj = j - Reftmpjj + n
-                    Refnn = Refw - 1 - n
-                    RefOutput[i,j] += Input[Refii][Refjj]*kernel[Refmm][Refnn]
+                    for d in range(0,Refindepth):
+                        Refjj = j - Reftmpjj + n
+                        Refnn = Refw - 1 - n
+                        RefOutput[i,j] += Input[Refii][Refjj][d]*kernel[Refmm][Refnn][d]
     return RefOutput
 
 
@@ -103,21 +69,22 @@ def refconvolution2D(Input, kernel):
 @click.command()
 @click.option('-rows', type=int, default=7)
 @click.option('-cols', type=int, default=7)
+@click.option('-indepth', type=int, default=3)
 @click.option('-w', type=int, default=3)
 @click.option('--version',
               type=click.Choice(
                   ('unoptimized','reference')),
               default='unoptimized')
 @click.option('--verify/--no-verify', default=True)
-def cli(rows, cols, w, version, verify):
+def cli(rows, cols, indepth, w, version, verify):
     """
     Different available versions:
     unoptimized: Run `convolution2D` without optimizations;
     """
 
     # Prepare data with numpy
-    Input = np.random.rand(rows, cols).astype(np_dtype)
-    kernel = np.random.rand(w, w).astype(np_dtype)
+    Input = np.random.rand(rows, cols, indepth).astype(np_dtype)
+    kernel = np.random.rand(w, w, indepth).astype(np_dtype)
     Output = np.zeros((rows, cols), dtype=np_dtype)
 
     # # Prepare data with numpy for debug
@@ -125,21 +92,21 @@ def cli(rows, cols, w, version, verify):
     # kernel = np.ones((w, w), dtype = np_dtype)
     # Output = np.zeros((rows, cols), dtype=np_dtype)
 
-    print(f'Convolution 2D {rows}x{cols}x{1} with kernel {w}x{w}(version: {version})')
+    print(f'Convolution 2D {rows}x{cols}x{indepth} with kernel {w}x{w}x{indepth}(version: {version})')
 
     if version == 'unoptimized':
         # print("skipped call")
         # Simply call the program to run it
-        convolution2D(Input, kernel, Output)
+        convolution3D(Input, kernel, Output)
     else:
         raise ValueError('Invalid version %s' % version)
 
     if verify:
-        print("Computed from dace")
-        print(Output)
+        #print("Computed from dace")
+        #print(Output)
         expected = refconvolution2D(Input, kernel)
-        print("Computed reference")
-        print(expected)
+        #print("Computed reference")
+        #print(expected)
         diff = np.linalg.norm(Output - expected) / (rows * cols)
         print('Difference:', diff)
         return 0 if diff <= 1e-6 else 1
