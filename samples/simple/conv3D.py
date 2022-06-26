aa199936b0e33e6f3837392b4abc0c66dd60b6bb
akanksha <abaranwal>
2022-06-26 17:53:24 +0200

    changing size parameters to integers from dace symbols

--- samples/simple/conv3D.py
+++ samples/simple/conv3D.py
@@ -13,42 +13,27 @@ import pandas as pd
 import json
 import timeit
 
-# For optimizations
-from dace.transformation.dataflow import (DoubleBuffering, MapCollapse, MapExpansion, MapReduceFusion, StripMining,
-                                          InLocalStorage, AccumulateTransient, Vectorization)
-from dace.transformation import helpers as xfutil
 
 # For library node implementations
 import dace.libraries.blas
 
 # Define symbolic sizes for arbitrary inputs
 # Convention used for 3D convolution height, width, depth, channels
-inchannels = dace.symbol('inchannels')
-indepth = dace.symbol('indepth')
-inheight = dace.symbol('inheight')
-inwidth = dace.symbol('inwidth')
-
-blockdim = dace.symbol('blockdim')
-
-N = dace.symbol('N') # Batch size
-
-outchannels = dace.symbol('outchannels')
-outdepth = dace.symbol('outdepth')
-outheight = dace.symbol('outheight')
-outwidth = dace.symbol('outwidth')
-
-#kdepth = dace.symbol('kdepth')
-kdepth = 3
-kheight = dace.symbol('kheight')
-kwidth = dace.symbol('kwidth')
-
+global indepth, inheight, inwidth
+global N, w
+global kwidth, kdepth, kheight
+global inchannels, outchannels
+global maxd, maxh, maxw
+global blockStep
+blockStep = 2
+N = 1
 # Define data type to use
 dtype = dace.float64
 np_dtype = np.float64
 
                 
 # Trying out some optimizations
-def optimize_for_gpu(sdfg: dace.SDFG, n: int, indepth: int, inheight: int, inwidth: int, inchannels: int, w: int, outchannels: int):
+def optimize_for_gpu(sdfg: dace.SDFG):
     """ Optimize 3D convolution example for GPUs. """
     # Ensure integers are 32-bit by default
     dace.Config.set('compiler', 'default_data_types', value='C')
@@ -58,9 +43,7 @@ def optimize_for_gpu(sdfg: dace.SDFG, n: int, indepth: int, inheight: int, inwid
 
 # Simple parallel 3D convolution
 @dace.program(device=dace.DeviceType.GPU)
-def dace_simple(Input: dtype[N, indepth, inheight, inwidth, inchannels],
-                      kernel: dtype[kdepth, kheight, kwidth, inchannels, outchannels],
-                      Output: dtype[N, indepth, inheight, inwidth, outchannels]):
+def dace_simple( Input, kernel, Output):
     Output[:] = 0
     for n, oc, d, h, w in dace.map[0:N, 0:outchannels, 0:indepth-kdepth+1, 0:inheight-kheight+1, 0:inwidth-kwidth+1]:
         tmp = np.zeros([1], dtype=Input.dtype)
@@ -68,70 +51,20 @@ def dace_simple(Input: dtype[N, indepth, inheight, inwidth, inchannels],
             tmp = tmp + Input[n, d+kd, h+kh, w+kw, ic] * kernel[kd, kh, kw, ic, oc]
         Output[n, d, h, w, oc] = tmp
 
-
-# # Loop tiling for outputs
-# # Simple parallel 3D convolution
-# TODO: This fails in the sdfg generation part specifically for GPU
 @dace.program(device=dace.DeviceType.GPU)
-def dace_tiling_v1(Input: dtype[N, indepth, inheight, inwidth, inchannels],
-                      kernel: dtype[kdepth, kheight, kwidth, inchannels, outchannels],
-                      Output: dtype[N, indepth, inheight, inwidth, outchannels]):
+def dace_tiling_2X2X2( Input, kernel, Output):
     Output[:] = 0
-    for n, oc, d, h, w in dace.map[0:N, 0:outchannels, 0:indepth-kdepth+1, 0:inheight-kheight+1, 0:inwidth-kwidth+1]:
-        tmp = np.zeros([1], dtype=Input.dtype)
-        localInput = np.copy(Input[n, d:d+kdepth, h:h+kheight, w:w+kwidth, 0:inchannels])
-        localKernel = np.copy(kernel)
-        for kd, kh, kw, ic in dace.map[0:kdepth, 0:kheight, 0:kwidth, 0:inchannels]:
-            tmp = tmp + localInput[kd, kh, kw, ic] * localKernel[kd, kh, kw, ic, oc]
-        Output[n, d, h, w, oc] = tmp
-
-# Take blocks of input and then compute all output channels one by one in the innermost loop. 
-# It should be like you pick up a certain set of inputs which are needed for a particular output value 
-# and then once its done only then you move on to the next set of inputs
-# TODO: This fails in the sdfg generation part specifically for GPU
-@dace.program(device=dace.DeviceType.GPU)
-def dace_tiling_v2(Input: dtype[N, indepth, inheight, inwidth, inchannels],
-                      kernel: dtype[kdepth, kheight, kwidth, inchannels, outchannels],
-                      Output: dtype[N, indepth, inheight, inwidth, outchannels]):
-    Output[:] = 0
-    for n in dace.map[0:N]:
-        localOutput = np.copy(Output[n, :, :, :, :])
-        for d, h, w in dace.map[ 0:indepth-kdepth+1, 0:inheight-kheight+1, 0:inwidth-kwidth+1]:
-            tmp = np.zeros([outchannels], dtype=Input.dtype)
-            localInput = np.copy(Input[n,d:d+kdepth, h:h+kheight, w:w+kwidth, 0:inchannels])
-            for oc in dace.map[0:outchannels]:
-                localKernel = np.copy(kernel[ :, :, :, :, oc])
-                for kd in range(0,kdepth): # The only compulsory sequential part
-                    for kh in range(0,kheight):
-                        for kw in range(0,kwidth):
-                            for ic in range(0,inchannels):
-                                tmp[oc] = tmp[oc] + localInput[kd, kh, kw, ic] * localKernel[kd, kh, kw, ic]
-            localOutput[d, h, w, :] = tmp[:]
-        Output[n,:,:,:,:] = localOutput
-
-
-# TODO: Figure out in which order should the innermost sequential loop be in 
-# TODO: Parameterize the block size
-@dace.program(device=dace.DeviceType.GPU)
-def dace_tiling_2X2X2(Input: dtype[N, indepth, inheight, inwidth, inchannels],
-                      kernel: dtype[kdepth, kheight, kwidth, inchannels, outchannels],
-                      Output: dtype[N, indepth, inheight, inwidth, outchannels]):
-    Output[:] = 0
-    for n in dace.map[0:N]:
-        localOutput = np.copy(Output[n, :, :, :, :])
-        for dhalf, hhalf, whalf, oc in dace.map[ 0:(indepth-kdepth+1)/2, 0:(inheight-kheight+1)/2, 0:(inwidth-kwidth+1)/2, 0:outchannels]:
-            localInputBlock = np.copy(Input[n, 2*dhalf:(2*dhalf+1)+kdepth, 2*hhalf:(2*hhalf+1)+kheight, 2*whalf:(2*whalf+1)+kwidth, 0:inchannels])
-            localKernel = np.copy(kernel[ :, :, :, :, oc])
-            tmpBlock = np.zeros([2,2,2], dtype=Input.dtype)
-            for wind, winh, winw in dace.map[0:2, 0:2, 0:2]:                    
-                localInput = np.copy(localInputBlock[ wind:wind+kdepth, winh:winh+kheight, winw:winw+kwidth, :])
-                for kd in range(0,kdepth): # The compulsory sequential part
-                    for kh in range(0,kheight):
-                        for kw in range(0,kwidth):
-                            for ic in range(0,inchannels):
-                                tmpBlock[wind, winh, winw] = tmpBlock[wind, winh, winw] + localInput[kd, kh, kw, ic] * localKernel[kd, kh, kw, ic]
-            localOutput[2*dhalf:2*dhalf+2, 2*hhalf:2*hhalf+2, 2*whalf:2*whalf+2, oc] = tmpBlock[:,:,:]
-        Output[n,:,:,:,:] = localOutput
+    for n, dparti, hparti, wparti, oc in dace.map[0:N, 0:maxd, 0:maxh, 0:maxw, 0:outchannels]:
+        localInputBlock = Input[n, blockStep*dparti:(blockStep*dparti+1)+kdepth, blockStep*hparti:(blockStep*hparti+1)+kheight, blockStep*wparti:(blockStep*wparti+1)+kwidth, 0:inchannels]
+        tmpBlock = np.zeros([blockStep,blockStep,blockStep], dtype=Input.dtype)
+        for wind, winh, winw in dace.map[0:blockStep, 0:blockStep, 0:blockStep]:
+            localInput = localInputBlock[ wind:wind+kdepth, winh:winh+kheight, winw:winw+kwidth, :]
+            for kd in range(0,kdepth): # The compulsory sequential part
+                for kh in range(0,kheight):
+                    for kw in range(0,kwidth):
+                        for ic in range(0,inchannels):
+                            tmpBlock[wind, winh, winw] = tmpBlock[wind, winh, winw] + kernel[kd, kh, kw, ic, oc] * localInput[kd, kh, kw, ic]
+        Output[n, blockStep*dparti:blockStep*(dparti+1), blockStep*hparti:blockStep*(hparti+1), blockStep*wparti:blockStep*(wparti+1), oc] = tmpBlock
 
 
 enableFun = [dace_tiling_2X2X2]
@@ -167,11 +100,15 @@ def timetfgpu_conv3D(input, filter):
     op=tf.nn.conv3d(input, filter, strides=[1, 1, 1, 1, 1], padding="VALID")
 
 # Verification
-def verify_with_ref_conv3D(dace_fun, refop, Input, kernel, Output, n, inchannels, outchannels, indepth, inheight, inwidth, w):
+def verify_with_ref_conv3D(dace_fun, refop, Input, kernel, Output):
+    global indepth, inheight, inwidth
+    global N
+    global kwidth, kdepth, kheight,w
+    global inchannels, outchannels
     dace_fun(Input, kernel, Output)
     opdace = tf.convert_to_tensor(Output)
     opdace = opdace[:, 0 : (indepth-w+1), 0 : (inheight-w+1) , 0 : (inwidth-w+1) , :]
-    diff = np.linalg.norm(opdace - refop) / (n * outchannels * indepth * inheight * inwidth)
+    diff = np.linalg.norm(opdace - refop) / (N * outchannels * indepth * inheight * inwidth)
     print('Difference:', diff)
     if(diff<=1e-6):
         print(f"Verification successfull")
@@ -192,7 +129,11 @@ def parsecsv(csv):
     return convparams
 
 def prepareinputs(currconv):
-    n = 1
+    global kwidth, kdepth, kheight
+    global w, N
+    global inchannels, outchannels
+    global indepth, inheight, inwidth
+    global maxd, maxh, maxw
     inchannels = currconv["InChannel"]
     indepth = currconv["InputDepth"]
     inheight = currconv["InputHeight"]
@@ -201,15 +142,18 @@ def prepareinputs(currconv):
     kheight = currconv["KernelHeight"]
     kwidth = currconv["KernelWidth"]
     outchannels = currconv["OutputChannel"]
+    maxd = int((indepth-kdepth+1)/blockStep)
+    maxh = int((inheight-kheight+1)/blockStep)
+    maxw = int((inwidth-kwidth+1)/blockStep)
 
     if (kdepth!=kheight or kheight!=kwidth or kdepth!=kwidth):
         print("ERROR: !!! Conv3D with unequal kernel spatial dimensions not implemented")
         return 1
     w = kdepth
     # Prepare data with numpy
-    Input = np.random.rand(n, indepth, inheight, inwidth, inchannels).astype(np_dtype)
+    Input = np.random.rand(N, indepth, inheight, inwidth, inchannels).astype(np_dtype)
     kernel = np.random.rand(w, w, w, inchannels, outchannels).astype(np_dtype)
-    Output = np.zeros((n, indepth, inheight, inwidth, outchannels), dtype=np_dtype)
+    Output = np.zeros((N, indepth, inheight, inwidth, outchannels), dtype=np_dtype)
     print(f'3D Convolution {inchannels}x{indepth}x{inheight}x{inwidth} '
             f'with kernel {outchannels}x{inchannels}x{w}x{w}x{w}')
 
@@ -226,9 +170,8 @@ def verifyconv3D(csv):
         input = tf.convert_to_tensor(Input)
         filter = tf.convert_to_tensor(kernel)
         op = tf.nn.conv3d(input, filter, strides=[1, 1, 1, 1, 1], padding="VALID")
-        n = 1
         for functionname in enableFun:
-            verify_with_ref_conv3D(functionname, op, Input, kernel, Output, n, currconv["InChannel"], currconv["OutputChannel"], currconv["InputDepth"], currconv["InputHeight"], currconv["InputWidth"], currconv["KernelWidth"])
+            verify_with_ref_conv3D(functionname, op, Input, kernel, Output)
         print("INFO: Verification done")
 
 
@@ -277,7 +220,12 @@ def benchmarkconv3D(csv, benchmark_fun, suffix, issdfg):
 
 
 def optimizewithsdfgfun(csv, dace_fun):
-    print("Will create sdfgs and you can optimize stuff")
+    print("Will create sdfgs to optimize")
+    global kwidth, kdepth, kheight
+    global w, N
+    global inchannels, outchannels
+    global indepth, inheight, inwidth
+    global maxd, maxh, maxw
     if csv == 'nocsv':
         n = 1
         indepth = 8
@@ -305,8 +253,8 @@ def optimizewithsdfgfun(csv, dace_fun):
             break
 
     sdfg_fun: dace.SDFG = dace_fun.to_sdfg()
-    optimize_for_gpu(sdfg_fun, n, indepth, inheight, inwidth, inchannels, w, outchannels)
-    sdfg_fun(Input,kernel,Output, N=n, indepth=indepth, inheight=inheight, inwidth=inwidth, inchannels=inchannels, kdepth=w, kheight=w, kwidth=w, outchannels=outchannels)
+    optimize_for_gpu(sdfg_fun)
+    sdfg_fun(Input,kernel,Output)
     return sdfg_fun
 
 @click.command()
