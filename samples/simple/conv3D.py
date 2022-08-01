1fbc3f2693838b8050f6d69a7a3e8685f025c6d7
akanksha <abaranwal>
2022-08-01 11:50:26 +0200

    backup of optimized code

--- samples/simple/conv3D.py
+++ samples/simple/conv3D.py
@@ -12,11 +12,16 @@ import os
 import pandas as pd
 import json
 import timeit
+from dace.sdfg.utils import load_precompiled_sdfg
 
+from dace.transformation.dataflow import (DoubleBuffering, MapCollapse, MapExpansion, MapReduceFusion, StripMining,
+                                          InLocalStorage, AccumulateTransient, Vectorization)
 
 # For library node implementations
 import dace.libraries.blas
 
+# TODO: Try out different tranformations and see its effect on the generated graph.
+# TODO: Fix different parts of your code to make it also work with parameters.
 # Define symbolic sizes for arbitrary inputs
 # Convention used for 3D convolution height, width, depth, channels
 global indepth, inheight, inwidth
@@ -26,48 +31,79 @@ global inchannels, outchannels
 global maxd, maxh, maxw
 global blockStep
 blockStep = 2
-N = 1
-# Define data type to use
-dtype = dace.float64
-np_dtype = np.float64
 
+N = 1
+
+# Define symbolic sizes for arbitrary inputs
+d_indepth = dace.symbol('indepth')
+d_inheight = dace.symbol('inheight')
+d_inwidth = dace.symbol('inwidth')
+
+# Define data type to use
+dtype = dace.float32
+np_dtype = np.float32
+
+n = 1
+indepth = 32
+inheight = 32
+inwidth = 32
+w = 3
+inchannels = 4
+outchannels = 8 
+# Prepare data with numpy
+Input = np.random.rand(n, indepth, inheight, inwidth, inchannels).astype(np_dtype)
+kernel = np.random.rand(w, w, w, inchannels, outchannels).astype(np_dtype)
+Output = np.zeros((n, indepth, inheight, inwidth, outchannels), dtype=np_dtype)
+kdepth = w
+kheight = w
+kwidth = w
+maxd = int((indepth-kdepth+1)/blockStep)
+maxh = int((inheight-kheight+1)/blockStep)
+maxw = int((inwidth-kwidth+1)/blockStep)
                 
 # Trying out some optimizations
 def optimize_for_gpu(sdfg: dace.SDFG):
     """ Optimize 3D convolution example for GPUs. """
     # Ensure integers are 32-bit by default
     dace.Config.set('compiler', 'default_data_types', value='C')
+    sdfg.simplify()
     # Apply GPU transformation
     sdfg.apply_gpu_transformations()
 
-
+# TODO: Parameterize the indepth, inheight, inwidth, inchannels, outchannels and then try out the optimized code
+# TODO: Clean up the code to remove unnecessary variables, functions etc. 
 # Simple parallel 3D convolution
-@dace.program(device=dace.DeviceType.GPU)
+@dace.program(device=dace.DeviceType.GPU, auto_optimize=True)
 def dace_simple( Input, kernel, Output):
-    Output[:] = 0
-    for n, oc, d, h, w in dace.map[0:N, 0:outchannels, 0:indepth-kdepth+1, 0:inheight-kheight+1, 0:inwidth-kwidth+1]:
-        tmp = np.zeros([1], dtype=Input.dtype)
-        for ic, kd, kh, kw in dace.map[0:inchannels, 0:kdepth, 0:kheight, 0:kwidth]:
-            tmp = tmp + Input[n, d+kd, h+kh, w+kw, ic] * kernel[kd, kh, kw, ic, oc]
-        Output[n, d, h, w, oc] = tmp
+    #Output[:] = 0
+    for n, d, h, w, oc in dace.map[0:N, 0:indepth-kdepth+1, 0:inheight-kheight+1, 0:inwidth-kwidth+1, 0:outchannels]:
+        r_tmp = np.zeros([1], dtype=Input.dtype)
+        r_kernel = np.copy(kernel[:,:,:,:,oc])
+        for kd, kh, kw, ic in dace.map[0:kdepth, 0:kheight, 0:kwidth, 0:inchannels]:
+            r_tmp = r_tmp + Input[n, d+kd, h+kh, w+kw, ic] * r_kernel[kd, kh, kw, ic]
+        Output[n, d, h, w, oc] = r_tmp
+
 
 @dace.program(device=dace.DeviceType.GPU)
 def dace_tiling_2X2X2( Input, kernel, Output):
     Output[:] = 0
-    for n, dparti, hparti, wparti, oc in dace.map[0:N, 0:maxd, 0:maxh, 0:maxw, 0:outchannels]:
-        localInputBlock = Input[n, blockStep*dparti:(blockStep*dparti+1)+kdepth, blockStep*hparti:(blockStep*hparti+1)+kheight, blockStep*wparti:(blockStep*wparti+1)+kwidth, 0:inchannels]
-        tmpBlock = np.zeros([blockStep,blockStep,blockStep], dtype=Input.dtype)
-        for wind, winh, winw in dace.map[0:blockStep, 0:blockStep, 0:blockStep]:
-            localInput = localInputBlock[ wind:wind+kdepth, winh:winh+kheight, winw:winw+kwidth, :]
-            for kd in range(0,kdepth): # The compulsory sequential part
-                for kh in range(0,kheight):
-                    for kw in range(0,kwidth):
-                        for ic in range(0,inchannels):
-                            tmpBlock[wind, winh, winw] = tmpBlock[wind, winh, winw] + kernel[kd, kh, kw, ic, oc] * localInput[kd, kh, kw, ic]
-        Output[n, blockStep*dparti:blockStep*(dparti+1), blockStep*hparti:blockStep*(hparti+1), blockStep*wparti:blockStep*(wparti+1), oc] = tmpBlock
+    for n, dparti, hparti, wparti, wind, winh, winw, oc in dace.map[0:N, 
+    0:maxd, 0:maxh, 0:maxw, 
+    0:blockStep, 0:blockStep, 0:blockStep, 
+    0:outchannels]:# Should be done in parallel
+        for kd, kh, kw, ic in dace.map[0:kdepth, 0:kheight, 0:kwidth, 0:inchannels]: #Compulsory sequential part
+            Output[n, 
+            blockStep*dparti+wind, 
+            blockStep*hparti+winh, 
+            blockStep*wparti+winw, 
+            oc] += kernel[kd, kh, kw, ic, oc] * Input[n,
+                                                    blockStep*dparti+wind+kd, 
+                                                    blockStep*hparti+winh+kh, 
+                                                    blockStep*wparti+winw+kw, 
+                                                    ic]
 
 
-enableFun = [dace_tiling_2X2X2]
+enableFun = [dace_simple]
 
 # Dace profiling method, Returns median values in ms
 def rundaceprofiling(dace_fun, Input, kernel, Output, reps):
@@ -83,12 +119,12 @@ def rundaceprofiling(dace_fun, Input, kernel, Output, reps):
 
 
 # Dace profiling method, Returns median values in ms
-def rundacesdfgprofiling(dace_fun, Input, kernel, Output, reps, n, indepth, inheight, inwidth, inchannels, w, outchannels):
+def rundacesdfgprofiling(dace_fun, Input, kernel, Output, reps):
     # Temporarily set the DACE_profiling config to True
     with dace.config.set_temporary('profiling', value=True):
         # You can control the number of times a program is run with the treps configuration
         with dace.config.set_temporary('treps', value=reps):
-            dace_fun(Input,kernel,Output, N=n, indepth=indepth, inheight=inheight, inwidth=inwidth, inchannels=inchannels, kdepth=w, kheight=w, kwidth=w, outchannels=outchannels)
+            dace_fun(Input=Input,kernel=kernel,Output=Output)
 
     list_of_files = glob.glob(f'.dacecache/*/profiling/results-*.csv')
     latest_file = max(list_of_files, key=os.path.getctime)
@@ -198,9 +234,8 @@ def benchmarkconv3D(csv, benchmark_fun, suffix, issdfg):
         # Dace Warmup
         if not (issdfg):
             rundaceprofiling(benchmark_fun, Input, kernel, Output, 10)
-        else:
-            
-            rundacesdfgprofiling(benchmark_fun, Input, kernel, Output, 10, n, indepth, inheight, inwidth, inchannels, w, outchannels)
+        else:            
+            rundacesdfgprofiling(benchmark_fun, Input, kernel, Output, 10)
         print("INFO: Warmup done")
 
         print("Start benchmarking instance")
@@ -243,16 +278,24 @@ def optimizewithsdfgfun(csv, dace_fun):
         for index, currconv in convparams.iterrows():
             Input, kernel, Output = prepareinputs(currconv)
             n = 1
+            N = 1
             inchannels = currconv["InChannel"]
             indepth = currconv["InputDepth"]
             inheight = currconv["InputHeight"]
             inwidth = currconv["InputWidth"]
             w = currconv["KernelDepth"]
             outchannels = currconv["OutputChannel"]
+
             print("For SDFGs code only parses first row of inputs")
             break
+    kdepth = w
+    kheight = w
+    kwidth = w
+    maxd = int((indepth-kdepth+1)/blockStep)
+    maxh = int((inheight-kheight+1)/blockStep)
+    maxw = int((inwidth-kwidth+1)/blockStep)
 
-    sdfg_fun: dace.SDFG = dace_fun.to_sdfg()
+    sdfg_fun: dace.SDFG = dace_fun.to_sdfg(Input,kernel,Output)
     optimize_for_gpu(sdfg_fun)
     sdfg_fun(Input,kernel,Output)
     return sdfg_fun
@@ -262,7 +305,7 @@ def optimizewithsdfgfun(csv, dace_fun):
 @click.option('--mode',
               type=click.Choice(
                   ('benchmarkoptimized', 'benchmarkunoptimized', 'verify', 'optimizeforgpu')),
-              default='verify')
+              default='optimizeforgpu')
 # Different available command lines are
 # --mode verify
 # --mode verify --csv sample
@@ -273,7 +316,7 @@ def optimizewithsdfgfun(csv, dace_fun):
 def cli(csv, mode):
     # Select which dace functions you want to enable
     # Select if you want it to read from a csv file and run the functions or if you want to hard code some values??
-    blockdim = 2
+
     if(csv == 'None'):
         csv = 'sample'
     if mode == 'benchmarkunoptimized':
@@ -294,7 +337,29 @@ def cli(csv, mode):
             iter = iter + 1
     elif mode == 'optimizeforgpu':
         for fun_name in enableFun:
-            sdfg_fun = optimizewithsdfgfun(csv, fun_name)
+            sdfg_fun: dace.SDFG = fun_name.to_sdfg(Input,kernel,Output)
+            optimize_for_gpu(sdfg_fun)
+            sdfg_fun(Input,kernel,Output)
+            print("Done calling optimize for gpu")
+        
+        # Prepare inputs for tensorflow fun
+        input = tf.convert_to_tensor(Input)
+        filter = tf.convert_to_tensor(kernel)
+        # TF warm up
+        timeit.Timer(lambda: timetfgpu_conv3D(input, filter)).repeat(repeat=5, number=1)
+        x = timeit.Timer(lambda: timetfgpu_conv3D(input, filter)).repeat(repeat=100, number=2)
+        print("Tensorflow in ms: ", (np.median(x))*1000)
+        sdfg_fun(Input,kernel,Output)
+        print("*\n*\n*")
+        print("Running code with no optimizations")        
+        rundacesdfgprofiling(sdfg_fun, Input, kernel, Output,10)
+        rundacesdfgprofiling(sdfg_fun, Input, kernel, Output,100)
+        print("*\n*\n*")
+        print("Running code with optimizations")
+        optimized_v1 = load_precompiled_sdfg('/home/akanksha/spcl/dacelocal/.dacecache/dace_simple_0/')
+        optimized_v1(Input=Input, kernel=kernel, Output=Output)
+        rundacesdfgprofiling(optimized_v1, Input, kernel, Output,10)
+        rundacesdfgprofiling(optimized_v1, Input, kernel, Output,100)    
     else:
         print("Not sure what you wanted to do. Choose between benchmarkunoptimized, verify and benchmarkoptimized")
     return 0
