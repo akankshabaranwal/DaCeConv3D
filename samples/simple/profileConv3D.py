174a790b4b715ec9490fd0ff2aa1898465f78a42
akanksha <abaranwal>
2022-09-07 10:51:52 +0200

    some additional profiling ways

--- samples/simple/profileConv3D.py
+++ samples/simple/profileConv3D.py
@@ -3,17 +3,24 @@
 from conv3D import *
 from daceml.testing.profiling import time_funcs, print_time_statistics
 
+# Flags to set kind of profiling
+verify = False
+profileendtoend = False
+profiletimefuncs = True
+nvproftf = False
+nvprofdace = False
+nvprofoptimizeddace = False
+nvtimefuncstf = True
+nvtimefuncsdace = False
+
 # Prepare inputs and kernel
 csv = 'cosmoflow'
 warmupiter = 10
 totaliter = 100
 convparams =  parsecsv(csv)
 layern = 0
-verify = False
-profileendtoend = False
-profiletimefuncs = True
 
-for layern in range(0,7):
+for layern in range(0,1):
     d_input, d_kernel, d_output, inchannels, indepth, inheight, inwidth, outchannels, batchsize = prepareinputs(convparams.iloc[layern])
 
     ## Prepare inputs for tensorflow fun
@@ -43,6 +50,18 @@ for layern in range(0,7):
     def run_optimized_dace():
         sdfg_fun(Input=d_input, kernel=d_kernel, Output=d_output,d_inchannels=inchannels, d_indepth=indepth, d_inheight=inheight,d_inwidth=inwidth, d_outchannels=outchannels, d_batchsize=batchsize)
 
+    # Dace profiling method, Returns median values in ms
+    def rundaceprofiling(run_dace_fun, reps):
+        # Temporarily set the DACE_profiling config to True
+        with dace.config.set_temporary('profiling', value=True):
+            # You can control the number of times a program is run with the treps configuration
+            with dace.config.set_temporary('treps', value=reps):
+                run_dace_fun()
+        list_of_files = glob.glob(f'.dacecache/*/profiling/results-*.csv')
+        latest_file = max(list_of_files, key=os.path.getctime)
+        df = pd.read_csv(latest_file)
+        return df['Runtime_sec']
+
     # Code for verification
     if verify:
         print("INFO: Running verification to compare against tensorflow output")
@@ -56,15 +75,57 @@ for layern in range(0,7):
         else:
             print(f"!!! ERROR: Incorrect verification")
 
-    # Profiling using time funcs
+    # Profiling tensorflow using nvprof
+    if nvproftf:
+        for i in range(0,warmupiter):
+            run_tf()
+        for i in range(0, totaliter):
+            run_tf()
+    
+    # Profiling baseline dace using nvprof
+    if nvprofdace:
+        for i in range(0, warmupiter):
+            run_dace()
+        for i in range(0, totaliter):
+            run_dace()
+
+    # Profiling optimized dace using nvprof
+    if nvprofoptimizeddace:
+        for i in range(0, warmupiter):
+            run_optimized_dace()
+        for i in range(0, totaliter):
+            run_optimized_dace()
+
+    # Comparitive profiling using time funcs
     if profiletimefuncs:
         print(f"INFO: Warmup for {warmupiter} iterations and total iterations {totaliter}")
-        times = time_funcs([run_dace, run_optimized_dace, run_tf],
-                        func_names=["baselinedace", "optimizeddace", "tf"],
+        times = time_funcs([run_optimized_dace, run_tf],
+                        func_names=["optimizeddace", "tf"],
                         warmups=warmupiter,
                         num_iters=totaliter)
         print(f"INFO: Statistics for layer number {layern}")
-        print_time_statistics(times, ["baselinedace", "optimizeddace", "tf"])
+        print_time_statistics(times, [ "optimizeddace", "tf"])
+
+    # nvprof using timefuncs for tf
+    if nvtimefuncstf:
+        print(f"INFO: Warmup for {warmupiter} iterations and total iterations {totaliter}")
+        times = time_funcs([run_tf],
+                        func_names=["tf"],
+                        warmups=warmupiter,
+                        num_iters=totaliter)
+        print(f"INFO: Statistics for layer number {layern}")
+        print_time_statistics(times, [ "tf"])
+
+    # nvprof using timefuncs for dace
+    if nvtimefuncsdace:
+        print(f"INFO: Warmup for {warmupiter} iterations and total iterations {totaliter}")
+        times = time_funcs([run_optimized_dace],
+                        func_names=["optimizeddace"],
+                        warmups=warmupiter,
+                        num_iters=totaliter)
+        print(f"INFO: Statistics for layer number {layern}")
+        print_time_statistics(times, [ "optimizeddace"])
+
 
     # Code for end to end runtime
     if profileendtoend:
@@ -76,72 +137,14 @@ for layern in range(0,7):
         tf_runtime = timeit.Timer(lambda: timetfgpu_conv3D(t_input, t_kernel)).repeat(repeat=100, number=2)
         print("INFO: Tensorflow in ms: ", (np.median(tf_runtime))*1000)
 
+        # print("*\n*\n*")
+        # print("INFO: Running baseline sdfg code")
+        # rundaceprofiling(run_dace, warmupiter)
+        # dace_runtime = rundaceprofiling(run_dace, totaliter)
+        # print("INFO: Dace time in ms: ",np.median(dace_runtime)*1000)
+
         print("*\n*\n*")
         print("INFO: Running optimized sdfg code")
-        rundacesdfgprofiling(sdfg_fun, d_input, d_kernel, d_output, inchannels, indepth, inheight, inwidth, outchannels, batchsize, warmupiter)
-        dace_optimized = rundacesdfgprofiling(sdfg_fun, d_input, d_kernel, d_output, inchannels, indepth, inheight, inwidth, outchannels, batchsize, totaliter)
+        rundaceprofiling(run_optimized_dace, warmupiter)
+        dace_optimized = rundaceprofiling(run_optimized_dace, totaliter)
         print("INFO: Dace time in ms: ",np.median(dace_optimized)*1000)
-
-
-# for layern in range(0,1):
-    
-#     d_input, d_kernel, d_output, inchannels, indepth, inheight, inwidth, outchannels, batchsize = prepareinputs(convparams.iloc[layern])
-
-#     t_input = tf.convert_to_tensor(d_input)
-#     t_kernel = tf.convert_to_tensor(d_kernel)
-
-#     inchannels = np.int32(inchannels)
-#     indepth = np.int32(indepth)
-#     inheight = np.int32(inheight)
-#     inwidth = np.int32(inwidth)
-#     outchannels = np.int32(outchannels)
-#     batchsize = np.int32(batchsize)
-
-#     # Function calls to dace_fun and tensorflow function
-#     def run_dace():
-#         dace_conv3d(Input=d_input, kernel=d_kernel, Output=d_output,d_inchannels=inchannels, d_indepth=indepth, d_inheight=inheight,d_inwidth=inwidth, d_outchannels=outchannels, d_batchsize=batchsize)
-
-#     def run_tf():
-#         op=tf.nn.conv3d(t_input, t_kernel, strides=[1, 1, 1, 1, 1], padding="VALID")
-
-#     # Optimize dace function
-#     sdfg_fun: dace.SDFG = dace_conv3d.to_sdfg(d_input, d_kernel, d_output)
-#     sdfg_fun.apply_gpu_transformations()
-#     optimize_for_gpu(sdfg_fun)
-
-#     # Function calls to run the optimized dace function
-#     def run_optimized_dace():
-#         sdfg_fun(Input=d_input, kernel=d_kernel, Output=d_output,d_inchannels=inchannels, d_indepth=indepth, d_inheight=inheight,d_inwidth=inwidth, d_outchannels=outchannels, d_batchsize=batchsize)
-
-#     if verify:
-#         print("INFO: Running verification to compare against tensorflow output")
-#         refop = tf.nn.conv3d(t_input, t_kernel, strides=[1, 1, 1, 1, 1], padding="VALID")
-#         sdfg_fun(Input=d_input, kernel=d_kernel, Output=d_output,d_inchannels=inchannels, d_indepth=indepth, d_inheight=inheight,d_inwidth=inwidth, d_outchannels=outchannels, d_batchsize=batchsize)
-#         opdace = tf.convert_to_tensor(d_output)
-#         diff = np.linalg.norm(opdace - refop) / (batchsize * outchannels * indepth * inheight * inwidth )
-#         print('Difference between tensorflow and dace values:', diff)
-#         if(diff<=1e-4):
-#             print(f"Verification successfull")
-#         else:
-#             print(f"!!! ERROR: Incorrect verification")
-
-#     if profiletimefuncs:
-#         print(f"INFO: Warmup for {warmupiter} iterations and total iterations {totaliter}")
-#         times = time_funcs([run_dace, run_optimized_dace, run_tf],
-#                         func_names=["baselinedace", "optimizeddace", "tf"],
-#                         warmups=warmupiter,
-#                         num_iters=totaliter)
-#         print(f"INFO: Statistics for layer number {layern}")
-#         print_time_statistics(times, ["baselinedace", "optimizeddace", "tf"])
-
-
-#     if profileendtoend:
-#         print(f"INFO: End to end profiling")
-#         print(f"INFO: Warmup for {warmupiter} iterations and total iterations {totaliter}")        
-#         ## TF warm up
-#         timeit.Timer(lambda: timetfgpu_conv3D(t_input, t_kernel)).repeat(repeat=10, number=1)
-#         tf_runtime = timeit.Timer(lambda: timetfgpu_conv3D(t_input, t_kernel)).repeat(repeat=100, number=2)
-#         print("INFO: Tensorflow in ms: ", (np.median(tf_runtime))*1000)
-#         rundacesdfgprofiling(sdfg_fun, d_input, d_kernel, d_kernel, inchannels, indepth, inheight, inwidth, outchannels, batchsize, warmupiter)
-#         dace_optimized = rundacesdfgprofiling(sdfg_fun, d_input, d_kernel, d_output, inchannels, indepth, inheight, inwidth, outchannels, batchsize, totaliter)
-#         print("INFO: Dace time in ms: ",np.median(dace_optimized)*1000)
