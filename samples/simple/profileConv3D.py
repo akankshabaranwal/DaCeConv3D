fbefe5af7be5a301b1ee6a0ccb3b0efad31b116f
Akanksha Baranwal <abaranwa@ault.cscs.ch>
2022-09-20 12:33:22 +0200

    adding summary plotting code

--- samples/simple/profileConv3D.py
+++ samples/simple/profileConv3D.py
@@ -8,17 +8,19 @@ import math
 import os
 import seaborn as sns
 import matplotlib.pyplot as plt
+import statistics
 
 parser = argparse.ArgumentParser(description='Process some integers.')
 parser.add_argument('-csv','--csv', type=str, default='cosmoflow', help='select which csv to profile')
 parser.add_argument('-warmupiter','--warmupiter', type=int, default=10, help='set number of warmup iterations')
 parser.add_argument('-totaliter','--totaliter', type=int, default=100, help='set number of total iterations')
 parser.add_argument('--verify', action='store_true', help='run verification')
-parser.add_argument('--comparetimefuncs', action='store_true', help='time funcs comparative summary time')
-parser.add_argument('--timefuncstf', action='store_true', help='run tf code with markers')
+parser.add_argument('--compareprof', action='store_true', help='time funcs comparative summary time')
+parser.add_argument('--proftf', action='store_true', help='run tf code with markers')
 parser.add_argument('--setlaunchwait', action='store_true', help='set launch wait')
-parser.add_argument('--timefuncsoptimdace', action='store_true', help='run dace code with markers')
+parser.add_argument('--profoptimdace', action='store_true', help='run dace code with markers')
 parser.add_argument('--plot', action='store_true', help='create plots')
+parser.add_argument('--enableplots', action='store_true', help='disable creating plots')
 # Charts
 # Create an outdir with output plots
 # Get violin plots of all of the data points
@@ -32,26 +34,28 @@ args = parser.parse_args()
 csv = args.csv
 convparams =  parsecsv(csv)
 verify = args.verify
-comparetimefuncs = args.comparetimefuncs
+compareprof = args.compareprof
 runtf = False
 rundace = False
 runoptimdace = False
-timefuncstf = args.timefuncstf
-timefuncsdace = False
-timefuncsoptimdace = args.timefuncsoptimdace
+proftf = args.proftf
+profdace = False
+profoptimdace = args.profoptimdace
 setlaunchwait = args.setlaunchwait
 warmupiter = args.warmupiter
 totaliter = args.totaliter
 currlayer = 0
+enableplots = args.enableplots
 lastlayer = convparams.shape[0]
 
-outdir = f'./outputplots/out{math.floor(time.time())}'
-os.mkdir(outdir)
-with open(f'./{outdir}/params.txt', 'w') as f:
-    f.writelines(f'csv: {csv}\n')
-    f.writelines(f'warmup iteration: {warmupiter}\n')
-    f.writelines(f'total iteration: {totaliter}\n')
-    f.writelines(f'set launch wait: {setlaunchwait}\n')
+if enableplots:
+    outdir = f'./outputplots/out{math.floor(time.time())}'
+    os.mkdir(outdir)
+    with open(f'./{outdir}/params.txt', 'w') as f:
+        f.writelines(f'csv: {csv}\n')
+        f.writelines(f'warmup iteration: {warmupiter}\n')
+        f.writelines(f'total iteration: {totaliter}\n')
+        f.writelines(f'set launch wait: {setlaunchwait}\n')
 
 args = parser.parse_args()
 d_input, d_kernel, d_output, inchannels, indepth, inheight, inwidth, outchannels, batchsize = prepareinputs(convparams.iloc[0])
@@ -100,6 +104,16 @@ def rundaceprofiling(run_dace_fun, reps):
     df = pd.read_csv(latest_file)
     return df['Runtime_sec']
 
+def run_fun(fun_name):
+    for i in range(0,warmupiter):
+        fun_name()
+    for i in range(0, totaliter):
+        fun_name()
+
+    
+median_dace = []
+median_tf = []
+layer_names = []
 
 for layern in range(currlayer, lastlayer):
     d_input, d_kernel, d_output, inchannels, indepth, inheight, inwidth, outchannels, batchsize = prepareinputs(convparams.iloc[layern])
@@ -116,6 +130,9 @@ for layern in range(currlayer, lastlayer):
     inwidth = np.int32(inwidth)
     outchannels = np.int32(outchannels)
     batchsize = np.int32(batchsize)
+    layer_name = f'in_{batchsize}X{indepth}X{inheight}X{inwidth}X{inchannels}_k_{kdim}X{kdim}X{kdim}_och_{outchannels}'
+    print(f'INFO: {layer_name}') 
+    layer_names.append(layer_name)
     
     # Code for verification
     if verify:
@@ -133,65 +150,98 @@ for layern in range(currlayer, lastlayer):
 
     # Profiling tensorflow using run
     if runtf:
-        for i in range(0,warmupiter):
-            run_tf()
-        for i in range(0, totaliter):
-            run_tf()
+        run_fun(run_tf)
     
     # Profiling baseline dace using run
     if rundace:
-        for i in range(0, warmupiter):
-            run_dace()
-        for i in range(0, totaliter):
-            run_dace()
+        run_fun(run_dace)
 
     # Profiling optimized dace using run
     if runoptimdace:
-        for i in range(0, warmupiter):
-            run_optim_dace()
-        for i in range(0, totaliter):
-            run_optim_dace()
+        run_fun(run_optim_dace)
 
     # Comparitive profiling using time funcs
     print(f"INFO: Warmup for {warmupiter} iterations and total iterations {totaliter}")
     print(f"INFO: Statistics for layer number {layern}")
-    if comparetimefuncs:
+    if compareprof:
         times = time_funcs([run_optim_dace, run_tf],
-                        func_names=["optimdace", "tf"],
+                        func_names=["dace", "tf"],
                         warmups=warmupiter,
                         num_iters=totaliter,
                         launch_wait=setlaunchwait)
-        print_time_statistics(times, [ "optimdace", "tf"])
-        # TODO: Summary plot and csv should get created here
-        # TODO: All comparison plots should be here
+        print_time_statistics(times, [ "dace", "tf"])
+        median_dace.append(statistics.median(times[0]))
+        median_tf.append(statistics.median(times[1]))
+        if enableplots:
+            d = {'tensorflow': pd.Series(times[1]), 'dace': pd.Series(times[0])}
+            df = pd.DataFrame(d)
 
-    # run using timefuncs for tf
-    if timefuncstf:
+            fig = plt.figure(figsize=(6, 6))
+            gs = fig.add_gridspec(1, 2)
+            ax = fig.add_subplot(gs[0, 0])
+            sns.violinplot(y=df["tensorflow"], cut=0)
+            ax.set(xlabel='tensorflow', ylabel='runtime in ms')
+
+            ax = fig.add_subplot(gs[0, 1])
+            sns.violinplot(y=df["dace"], cut=0)
+            ax.set(xlabel='dace', ylabel='runtime in ms')
+
+            fig.tight_layout()
+            fig = ax.get_figure()
+
+            figurename = f'{outdir}/allviolin_{layer_name}.png'
+            fig.savefig(figurename)
+            
+
+    # run using prof for tf
+    if proftf:
         times = time_funcs([run_tf],
                         func_names=["tf"],
                         warmups=warmupiter,
                         num_iters=totaliter,
                         launch_wait=setlaunchwait)
         print_time_statistics(times, [ "tf"])
+        median_tf.append(statistics.median(times[1]))
         # TODO: All individual violin plots for each layer
         # TODO: Fix the violin plot if its showing negative axis as well 
 
-     # run using timefuncs for dace
-    if timefuncsdace:
-        times = time_funcs([run_dace],
-                        func_names=["baselinedace"],
-                        warmups=warmupiter,
-                        num_iters=totaliter,
-                        launch_wait=setlaunchwait)
-        print_time_statistics(times, [ "baselinedace"])
-
-    # run using timefuncs for dace
-    if timefuncsoptimdace:
+    # run using prof for dace
+    if profoptimdace:
         times = time_funcs([run_optim_dace],
                         func_names=["optimdace"],
                         warmups=warmupiter,
                         num_iters=totaliter,
                         launch_wait=setlaunchwait)
         print_time_statistics(times, [ "optimdace"])
+        median_dace.append(statistics.median(times[0]))
         # TODO: All individual violin plots for each layer
-        # TODO: Fix the violin plot if its showing negative axis as well 
+        # TODO: Fix the violin plot if its showing negative axis as well
+    
+if enableplots: 
+    if len(median_dace) != 0 and len(median_tf) !=0:
+        print("INFO: Plotting summary graph")
+        # set width of bar
+        barWidth = 0.2
+        fig = plt.subplots(figsize =(12, 8))
+        # Set position of bar on X axis
+        br1 = np.arange(len(median_tf))
+        br2 = [x + barWidth for x in br1]
+        
+        # Make the plot
+        plt.bar(br1, median_tf, color ='r', width = barWidth,
+                edgecolor ='grey', label ='tensorflow')
+        plt.bar(br2, median_dace, color ='g', width = barWidth,
+                edgecolor ='grey', label ='dace')
+        
+        # Adding Xticks
+        plt.xlabel('Variation across different dimensions', fontweight ='bold', fontsize = 15)
+        plt.ylabel('Log of median runtime in ms', fontweight ='bold', fontsize = 15)
+        plt.xticks([r + barWidth for r in range(len(median_dace))], layer_names)
+        
+        plt.legend()
+        plt.yscale('log')
+        plt.xticks(rotation=45, ha='right')
+        plt.savefig(f'{outdir}/median_runtime', bbox_inches='tight')
+
+    elif len(median_dace)!=0 or len(median_tf)!=0:
+        print("INFO: Plot single function graph")
