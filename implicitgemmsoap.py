46f2804285a6ca8034b3e264ffb896db1b3f0ff9
Akanksha Baranwal <abaranwa@ault.cscs.ch>
2022-11-14 18:30:47 +0100

    only dace fails verif

--- implicitgemmsoap.py
+++ implicitgemmsoap.py
@@ -15,20 +15,20 @@ d_kdim = dace.symbol('d_kdim')
 dtype = dace.float32
 np_dtype = np.float32
 
-inchannels = 4
+inchannels = 1
 indepth = 5
 inheight = 5
 inwidth = 5
-outchannels = 2
+outchannels = 1
 kdim = 3
 outdepth = indepth - kdim + 1
 outheight = inheight - kdim + 1
 outwidth = inheight - kdim + 1
-batchsize = 2
+batchsize = 1
 dace.Config.set('compiler', 'default_data_types', value='C')
 
 # Direct convolution for verification
-def direct_conv3d(Input, kernel, Output):
+def direct_conv3d(direct_input, direct_kernel, direct_output):
     for n in range(0, batchsize):
         for d in range(0, outdepth):
             for h in range(0, outheight):
@@ -39,15 +39,14 @@ def direct_conv3d(Input, kernel, Output):
                             for kh in range(0, kdim):
                                 for kw in range(0, kdim):
                                     for ic in range(0, inchannels):
-                                        tmp += Input[ n, d+kd, h+kh, w+kw, ic]*kernel[oc, kd, kh, kw, ic]
-                        Output[n, d, h, w, oc] = tmp
+                                        tmp += direct_input[ n, d+kd, h+kh, w+kw, ic]*direct_kernel[oc, kd, kh, kw, ic]
+                        direct_output[n, d, h, w, oc] = tmp
 
 
 # GEMM_M = d_batchsize * d_outdepth * d_outheight * d_outwidth
 # GEMM_N = d_outchannels
 # GEMM_K = d_inchannels * d_kdim * d_kdim * d_kdim
-def implicit_gemm_conv3d(Input, kernel, Output):
-
+def implicit_gemm_conv3d(imgemm_input, imgemm_kernel, imgemm_output):
     for gemm_i, gemm_j in dace.map[0:batchsize*outdepth*outheight*outwidth, 0:outchannels]:
         n = gemm_i//(outdepth*outheight*outwidth)
         nopq_residual = gemm_i % (outdepth*outheight*outwidth)
@@ -75,22 +74,22 @@ def implicit_gemm_conv3d(Input, kernel, Output):
             h = p + r
             w = q + s
 
-            a = Input[n, d, h, w, c]
-            b = kernel[k, t, r, s, c]
+            a = imgemm_input[n, d, h, w, c]
+            b = imgemm_kernel[k, t, r, s, c]
             accum[0] = accum[0] + a*b
 
-        Output[ n, o, p, q, k] = accum[0]
+        imgemm_output[ n, o, p, q, k] = accum[0]
 
 
 # GEMM_M = d_batchsize * d_outdepth * d_outheight * d_outwidth
 # GEMM_N = d_outchannels
 # GEMM_K = d_inchannels * d_kdim * d_kdim * d_kdim
 @dace.program(device=dtypes.DeviceType.GPU, auto_optimize=True)
-def dace_implicit_gemm_conv3d(Input: dtype[d_batchsize, d_outdepth+d_kdim-1, d_outheight+d_kdim-1, d_outwidth+d_kdim-1, d_inchannels] @dace.StorageType.GPU_Global ,
-                kernel: dtype[d_outchannels, d_kdim, d_kdim, d_kdim, d_inchannels] @dace.StorageType.GPU_Global,
-                Output: dtype[d_batchsize, d_outdepth, d_outheight, d_outwidth, d_outchannels] @dace.StorageType.GPU_Global):
+def dace_implicit_gemm_conv3d(dace_input: dtype[d_batchsize, d_outdepth+d_kdim-1, d_outheight+d_kdim-1, d_outwidth+d_kdim-1, d_inchannels] @dace.StorageType.GPU_Global ,
+                dace_kernel: dtype[d_outchannels, d_kdim, d_kdim, d_kdim, d_inchannels] @dace.StorageType.GPU_Global,
+                dace_output: dtype[d_batchsize, d_outdepth, d_outheight, d_outwidth, d_outchannels] @dace.StorageType.GPU_Global):
    
-    for gemm_i, gemm_j in dace.map[0:d_batchsize*d_outdepth*d_outheight*d_outwidth, 0:d_outchannels]:
+    for gemm_i, gemm_j in dace.map[0:(d_batchsize*d_outdepth*d_outheight*d_outwidth), 0:d_outchannels]:
         n = gemm_i//(d_outdepth*d_outheight*d_outwidth)
         nopq_residual = gemm_i % (d_outdepth*d_outheight*d_outwidth)
         
@@ -100,10 +99,10 @@ def dace_implicit_gemm_conv3d(Input: dtype[d_batchsize, d_outdepth+d_kdim-1, d_o
         p = opq_residual//d_outwidth
         q = opq_residual%d_outwidth
         
-        accum = np.zeros([1], dtype=Input.dtype)
+        accum = np.zeros([1], dtype=dace_input.dtype)
         k = gemm_j
         
-        for gemm_k in dace.map[0: d_inchannels * d_kdim * d_kdim * d_kdim]:
+        for gemm_k in dace.map[0: (d_inchannels * d_kdim * d_kdim * d_kdim)]:
             c = gemm_k//(d_kdim*d_kdim*d_kdim)
             ctrs_residual = gemm_k%(d_kdim*d_kdim*d_kdim)
 
@@ -117,20 +116,20 @@ def dace_implicit_gemm_conv3d(Input: dtype[d_batchsize, d_outdepth+d_kdim-1, d_o
             h = p + r
             w = q + s
 
-            a = Input[n, d, h, w, c]
-            b = kernel[k, t, r, s, c]
+            a = dace_input[n, d, h, w, c]
+            b = dace_kernel[k, t, r, s, c]
             accum[0] = accum[0] + a*b
 
-        Output[ n, o, p, q, k] = accum[0]
+        dace_output[ n, o, p, q, k] = accum[0]
 
 dace_input = torch.rand(batchsize, indepth, inheight, inwidth, inchannels).cuda()
 dace_kernel = torch.rand(outchannels, kdim, kdim, kdim, inchannels).cuda()
 dace_output = torch.zeros(batchsize, outdepth, outheight, outwidth, outchannels).cuda()
-imgemm_input = dace_input.detach().clone()
-imgemm_kernel = dace_kernel.detach().clone()
+imgemm_input = dace_input.detach().cpu().numpy()
+imgemm_kernel = dace_kernel.detach().cpu().numpy()
 imgemm_output = torch.zeros(batchsize, outdepth, outheight, outwidth, outchannels).cuda()
-direct_input = dace_input.detach().clone()
-direct_kernel = dace_kernel.detach().clone()
+direct_input = dace_input.detach().cpu().numpy()
+direct_kernel = dace_kernel.detach().cpu().numpy()
 direct_output = torch.zeros(batchsize, outdepth, outheight, outwidth, outchannels).cuda()
 
 # #TODO: Figure out why this doesn't work. Like without converting to sdfg, the call to dace implicit gemm convolution fails.
@@ -138,15 +137,16 @@ direct_output = torch.zeros(batchsize, outdepth, outheight, outwidth, outchannel
 sdfg_fun_conv3d: dace.SDFG = dace_implicit_gemm_conv3d.to_sdfg(dace_input, dace_kernel, dace_output)
 sdfg_fun_conv3d.apply_gpu_transformations()
 
-sdfg_fun_conv3d(Input=dace_input, kernel=dace_kernel, Output=dace_output, 
+sdfg_fun_conv3d(dace_input=dace_input, dace_kernel=dace_kernel, dace_output=dace_output, 
              d_inchannels = inchannels, d_batchsize = batchsize, d_outchannels = outchannels,
              d_outdepth = outdepth, d_outheight = outheight, d_outwidth = outwidth, 
              d_kdim = kdim)
-
-# diff = np.linalg.norm(imgemm_output.cpu() - dace_output.cpu()) / (batchsize * outchannels * indepth * inheight * inwidth )
-# print('Difference between implicit gemm and dace converted values:', diff)
-
 implicit_gemm_conv3d(imgemm_input, imgemm_kernel, imgemm_output)
+print(f'dace output is: {dace_output.cpu()}')
+print(f'imgemm output is: {imgemm_output.cpu()}')
+diff = np.linalg.norm(imgemm_output.cpu() - dace_output.cpu()) / (batchsize * outchannels * indepth * inheight * inwidth )
+print('Difference between implicit gemm and dace converted values:', diff)
+
 direct_conv3d(direct_input, direct_kernel, direct_output)
 diff = np.linalg.norm(imgemm_output.cpu() - direct_output.cpu()) / (batchsize * outchannels * outdepth * outheight * outwidth )
 print('Difference between implicit gemm and direct conv values:', diff)
