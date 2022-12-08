backup of fixing computation hierarchy in assignM

samples/simple/convutils.py
@@ -99,7 +99,7 @@ def parsecsv(csv):
 
 
 # Data layout is NCDHW for pytorch
-def prepareinputs(currconv, layout):
+def prepareinputs(currconv, layout, batchsize):
     inchannels = currconv["InChannel"]
     indepth = currconv["InputDepth"]
     inheight = currconv["InputHeight"]
@@ -109,7 +109,7 @@ def prepareinputs(currconv, layout):
     outdepth = indepth - kdim + 1
     outheight = inheight - kdim + 1
     outwidth = inheight - kdim + 1
-    batchsize = 16 # Maximum experimentally runnable batch size is 16. Theoretical is 64.
+    #batchsize = 16 # Maximum experimentally runnable batch size is 16. Theoretical is 64.
     if layout == 'NCDHW':
         # Prepare data with pytorch
         Input = torch.rand(batchsize, inchannels, indepth, inheight, inwidth).cuda()

samples/simple/implicitGemmdace.py
@@ -2,7 +2,10 @@ import dace
 import numpy as np
 from dace import dtypes
 from dace.transformation.interstate import StateFusion
-from dace.transformation.dataflow import TaskletFusion, MapReduceFusion
+from dace.transformation.dataflow import TaskletFusion, MapReduceFusion, MapCollapse, MapTiling, StripMining
+from convutils import find_map_by_param
+from dace.transformation import helpers as xfutil
+
 
 # Define symbolic sizes for arbitrary inputs
 d_outdepth = dace.symbol('d_outdepth')
@@ -31,13 +34,36 @@ def optimize_for_gpu(sdfg: dace.SDFG):
     dace.Config.set('compiler', 'default_data_types', value='C')
     # Fuse the map and reduce nodes
     # Apply GPU transformation
-    #sdfg.apply_transformations_repeated(MapReduceFusion)
+    sdfg.apply_transformations(MapReduceFusion)
     sdfg.apply_gpu_transformations()
-    #sdfg.apply_transformations_repeated(TaskletFusion)
+    
+    entry_i = find_map_by_param(sdfg, 'gemm_i')
+    xfutil.tile(sdfg, entry_i, False, False, gemm_i=64, gemm_j=16) # CTA tiling Old values: 64,16 
+    entry_k = find_map_by_param(sdfg, 'gemm_k')
+    xfutil.tile(sdfg, entry_k, False, False, gemm_k=8) # CTA tiling Old values: 64,16
+    #xfutil.tile(sdfg, entry_i, False, False, gemm_i=8, gemm_j=4) # Warp tiling Old values: 8, 4
+
+    # # Collapse tiled gemm_i and gemm_j maps
+    # gtile_i = find_map_by_param(sdfg, 'tile_gemm_i')
+    # gtile_j = find_map_by_param(sdfg, 'tile_gemm_j')
+    # MapCollapse.apply_to(sdfg, outer_map_entry=gtile_i, inner_map_entry=gtile_j, permissive=True)
+    
+    # # Collapse inner gemm_i and gemm_j maps
+    # btile_i = find_map_by_param(sdfg, 'tile1_gemm_i')
+    # btile_j = find_map_by_param(sdfg, 'tile1_gemm_j')
+    # MapCollapse.apply_to(sdfg, outer_map_entry=btile_i, inner_map_entry=btile_j, permissive=True)
+    
+    # btile = find_map_by_param(sdfg, 'tile1_gemm_i')
+    # btile.map.schedule = dace.ScheduleType.GPU_ThreadBlock
+
+    return
+
+    #xfutil.tile(sdfg, entry_k, False, False, gemm_k=4)
+    
     return
 
 @dace.program(device=dtypes.DeviceType.GPU, auto_optimize=True)
-def dace_conv3d_baseline(Input: dtype[d_batchsize, d_outdepth+d_kdim-1, d_outheight+d_kdim-1, d_outwidth+d_kdim-1, d_inchannels] @dace.StorageType.GPU_Global ,
+def dace_conv3d(Input: dtype[d_batchsize, d_outdepth+d_kdim-1, d_outheight+d_kdim-1, d_outwidth+d_kdim-1, d_inchannels] @dace.StorageType.GPU_Global ,
                 kernel: dtype[d_outchannels, d_kdim, d_kdim, d_kdim, d_inchannels] @dace.StorageType.GPU_Global,
                 Output: dtype[d_batchsize, d_outdepth, d_outheight, d_outwidth, d_outchannels] @dace.StorageType.GPU_Global):
     
@@ -77,107 +103,3 @@ def dace_conv3d_baseline(Input: dtype[d_batchsize, d_outdepth+d_kdim-1, d_outhei
             accum = accum + Input[n, d, h, w, c]*kernel[gemm_j, t, r, s, c]
 
         Output[ n, o, p, q, gemm_j] = accum
-    
-
-
-CTAtileM = 4
-CTAtileN = 4
-CTAtileK = 4
-
-WARPtileM = 2
-WARPtileN = 2
-WARPtileK = 2
-
-# Tile based on matrix multiplication code
-@dace.program(device=dtypes.DeviceType.GPU, auto_optimize=True)
-def dace_conv3d_bad(Input: dtype[d_batchsize, d_outdepth+d_kdim-1, d_outheight+d_kdim-1, d_outwidth+d_kdim-1, d_inchannels] @dace.StorageType.GPU_Global ,
-                kernel: dtype[d_outchannels, d_kdim, d_kdim, d_kdim, d_inchannels] @dace.StorageType.GPU_Global,
-                Output: dtype[d_batchsize, d_outdepth, d_outheight, d_outwidth, d_outchannels] @dace.StorageType.GPU_Global):
-    
-    d_GEMM_M = (d_batchsize*d_outdepth*d_outheight*d_outwidth)
-    d_GEMM_N = d_outchannels
-    d_GEMM_K = (d_inchannels * d_kdim * d_kdim * d_kdim)
-    DHW = d_outdepth*d_outheight*d_outwidth
-    HW = d_outheight*d_outwidth
-    kdim3 = d_kdim*d_kdim*d_kdim
-    kdim2 = d_kdim*d_kdim
-
-    tmp_splitk = np.ndarray([d_batchsize*d_outdepth*d_outheight*d_outwidth, d_outchannels, d_inchannels * d_kdim * d_kdim * d_kdim], dtype=Input.dtype)
-
-    for gemm_i, gemm_j, gemm_k in dace.map[0:d_GEMM_M, 0:d_GEMM_N, 0:d_GEMM_K]:
-        n, nopq_residual = dace.int32(gemm_i/DHW), dace.int32(gemm_i % DHW)
-        o, opq_residual = dace.int32(nopq_residual/HW), dace.int32(nopq_residual%HW)
-        p, q = dace.int32(opq_residual/d_outwidth), dace.int32(opq_residual%d_outwidth)
-
-        c, ctrs_residual = dace.int32(gemm_k/kdim3), dace.int32(gemm_k%kdim3)
-        t, trs_residual = dace.int32(ctrs_residual/kdim2), dace.int32(ctrs_residual%kdim2)
-        r, s = dace.int32(trs_residual/d_kdim), dace.int32(trs_residual%d_kdim)
-
-        d, h, w = (o + t), (p + r), (q + s)
-
-        tmp_splitk[gemm_i, gemm_j, gemm_k] = Input[n, d, h, w, c]*kernel[gemm_j, t, r, s, c]
-
-    tmp_reducedk = np.ndarray([d_batchsize*d_outdepth*d_outheight*d_outwidth, d_outchannels], dtype=Input.dtype)
-    dace.reduce(lambda a, b: a + b, tmp_splitk, tmp_reducedk, axis=2, identity=0)
-    for gemm_i, gemm_j in dace.map[0:d_GEMM_M, 0:d_GEMM_N]:
-        n, nopq_residual = dace.int32(gemm_i/DHW), dace.int32(gemm_i % DHW)
-        o, opq_residual = dace.int32(nopq_residual/HW), dace.int32(nopq_residual%HW)
-        p, q = dace.int32(opq_residual/d_outwidth), dace.int32(opq_residual%d_outwidth)
-        Output[ n, o, p, q, gemm_j] = tmp_reducedk[gemm_i, gemm_j]
-
-CTAtileM = 4
-CTAtileN = 4
-CTAtileK = 4
-
-WARPtileM = 2
-WARPtileN = 2
-WARPtileK = 2
-
-
-# Tiling and buffering
-#TODO: Check what happens if you change the kdim variables to constants instead of dace map.
-@dace.program(device=dtypes.DeviceType.GPU, auto_optimize=True)
-def dace_conv3d(Input: dtype[d_batchsize, d_outdepth+d_kdim-1, d_outheight+d_kdim-1, d_outwidth+d_kdim-1, d_inchannels] @dace.StorageType.GPU_Global ,
-                kernel: dtype[d_outchannels, d_kdim, d_kdim, d_kdim, d_inchannels] @dace.StorageType.GPU_Global,
-                Output: dtype[d_batchsize, d_outdepth, d_outheight, d_outwidth, d_outchannels] @dace.StorageType.GPU_Global):
-    
-    d_GEMM_M = (d_batchsize*d_outdepth*d_outheight*d_outwidth)
-    d_GEMM_N = d_outchannels
-    d_GEMM_K = (d_inchannels * d_kdim * d_kdim * d_kdim)
-    d_DHW = d_outdepth*d_outheight*d_outwidth
-    d_HW = d_outheight*d_outwidth
-    d_kdim3 = d_kdim*d_kdim*d_kdim
-    d_kdim2 = d_kdim*d_kdim
-
-    for cta_n, cta_m in dace.map[0:d_GEMM_N:CTAtileN, 0:d_GEMM_M:CTAtileM]:
-            cta_reducedk = np.zeros([CTAtileM, CTAtileN], dtype=Input.dtype)
-            for cta_k in dace.map[0:d_GEMM_K:CTAtileK]:
-                cta_splitk = np.zeros([CTAtileM, CTAtileN], dtype=Input.dtype)
-
-                for warp_n, warp_m in dace.map[0: CTAtileN:WARPtileN, 0: CTAtileM:WARPtileM]:
-                        warp_reducedk = np.zeros([WARPtileM, WARPtileN], dtype=Input.dtype)
-                        for warp_k in dace.map[0:CTAtileK:WARPtileK]:
-                            warp_splitk = np.zeros([WARPtileM, WARPtileN], dtype=Input.dtype)
-                            
-                            for gemm_k in dace.map[0: WARPtileK]:
-                                for gemm_m, gemm_n in dace.map[0:WARPtileM, 0:WARPtileN]:
-                                            n, nopq_residual =  dace.int32((gemm_m+cta_m+warp_m)/d_DHW), dace.int32((gemm_m+cta_m+warp_m) % d_DHW)
-                                            o, opq_residual = dace.int32(nopq_residual/d_HW), dace.int32(nopq_residual%d_HW)
-                                            p, q = dace.int32(opq_residual/d_outwidth), dace.int32(opq_residual%d_outwidth)
-
-                                            c, ctrs_residual = dace.int32((gemm_k+cta_k+warp_k)/d_kdim3), dace.int32((gemm_k+cta_k+warp_k)%d_kdim3)
-                                            t, trs_residual = dace.int32(ctrs_residual/d_kdim2), dace.int32(ctrs_residual%d_kdim2)
-                                            r, s = dace.int32(trs_residual/d_kdim), dace.int32(trs_residual%d_kdim)
-                                            d, h, w = o + t, p + r, q + s
-
-                                            warp_splitk[gemm_m, gemm_n] = Input[n, d, h, w, c]*kernel[gemm_n+cta_n+warp_n, t, r, s, c]
-                                warp_reducedk = warp_reducedk + warp_splitk
-                        for tmp_m, tmp_n in dace.map[0: WARPtileM, 0:WARPtileN]:
-                                cta_splitk[tmp_m+warp_m, warp_n+tmp_n] = warp_reducedk[tmp_m, tmp_n]
-                cta_reducedk = cta_reducedk+cta_splitk
-
-            for assign_m, assign_n in dace.map[0:CTAtileM, 0:CTAtileN]:
-                    n, nopq_residual = dace.int32((cta_m+assign_m)/d_DHW), dace.int32((cta_m+assign_m) % d_DHW)
-                    o, opq_residual = dace.int32(nopq_residual/d_HW), dace.int32(nopq_residual%d_HW)        
-                    p, q = dace.int32(opq_residual/d_outwidth), dace.int32(opq_residual%d_outwidth)
-                    Output[ n, o, p, q, cta_n+assign_n] = cta_reducedk[assign_m, assign_n]

samples/simple/profileConv3D.py
@@ -34,6 +34,8 @@ parser.add_argument('-totaliter','--totaliter', type=int, default=100, help='set
 parser.add_argument('-lastlayer','--lastlayer', type=int, default=1, help='set number of total iterations')
 parser.add_argument('-currlayer','--currlayer', type=int, default=0, help='set number of total iterations')
 
+parser.add_argument('-implementation','--implementation', type=str, default='implicitGemmTileddace', help='select which implementation to run')
+
 # TODO: Check if you can plot and compare different versions of dace optimizations
 # TODO: Automate the roofline analysis plot
 # FIXME: Something is wrong with subplot when the csv file has just one row  
@@ -59,6 +61,8 @@ currlayer = args.currlayer
 enableplots = args.enableplots
 lastlayer = min(args.lastlayer, convparams.shape[0])
 
+batchsizes = [2]
+
 if (verify and compareprof):
     sys.exit("!!! ERROR: Something pycuda context issue when both verif and compareprof are called together")
 
@@ -68,7 +72,10 @@ torch.cuda.empty_cache()
 # Select the dace implementation to run
 #selectMethod = 'directConvNCDHWdace'
 #selectMethod = 'directConvNDHWCdace' 
-selectMethod = 'implicitGemmdace' #Verified working with batch size 16 on GV100
+#selectMethod = 'implicitGemmTileddace' #Verified working with batch size 16 on GV100
+#selectMethod = 'implicitGemmdace' #Baseline implicit gemm implementation
+
+selectMethod = args.implementation
 
 if selectMethod == 'directConvNCDHWdace':
     from directConvNCDHWdace import *
@@ -79,11 +86,17 @@ elif selectMethod == 'directConvNDHWCdace':
 elif selectMethod == 'implicitGemmdace':
     from implicitGemmdace import *
     layout = 'NDHWC'
+elif selectMethod == 'implicitGemmTileddace':
+    from implicitGemmTileddace import *
+    layout = 'NDHWC'
 else:
     sys.exit("!!ERROR: Select valid dace implementation")
 
-outdir = f'./outputplots/{selectMethod}_out'
-#outdir = f'./outputplots/out{math.floor(time.time())}'
+import math
+import time
+
+#outdir = f'./outputplots/{selectMethod}_out'
+outdir = f'./outputplots/out_{selectMethod}_{batchsizes[0]}'
 #os.mkdir(outdir)
 with open(f'./{outdir}/params.txt', 'w') as f:
     f.writelines(f'csv: {paramscsv}\n')
@@ -92,7 +105,7 @@ with open(f'./{outdir}/params.txt', 'w') as f:
     f.writelines(f'set launch wait: {setlaunchwait}\n')
 
 args = parser.parse_args()
-d_input, d_kernel, d_output, inchannels, indepth, inheight, inwidth, outchannels, batchsize, kdim = prepareinputs(convparams.iloc[0], layout)
+d_input, d_kernel, d_output, inchannels, indepth, inheight, inwidth, outchannels, batchsize, kdim = prepareinputs(convparams.iloc[0], layout, batchsizes[0])
 
 ## Prepare inputs for pytorch fun
 t_input = d_input.clone()
@@ -150,108 +163,109 @@ summary = []
 in_desc, out_desc, filt_desc, ws_ptr = destroydescinoutfilt(in_desc, out_desc, filt_desc, ws_ptr)
 
 for layern in range(currlayer, lastlayer):
-    d_input, d_kernel, d_output, inchannels, indepth, inheight, inwidth, outchannels, batchsize, kdim = prepareinputs(convparams.iloc[layern], layout)
-    layersummary = {}
-    t_input = d_input.clone()
-    t_kernel = d_kernel.clone()
+    for batchsize in batchsizes:
+        d_input, d_kernel, d_output, inchannels, indepth, inheight, inwidth, outchannels, batchsize, kdim = prepareinputs(convparams.iloc[layern], layout, batchsize)
+        layersummary = {}
+        t_input = d_input.clone()
+        t_kernel = d_kernel.clone()
 
-    inchannels = np.int32(inchannels)
-    indepth = np.int32(indepth)
-    inheight = np.int32(inheight)
-    inwidth = np.int32(inwidth)
-    outchannels = np.int32(outchannels)
-    batchsize = np.int32(batchsize)
-    outdepth = np.int32(indepth - kdim + 1)
-    outheight = np.int32(inheight - kdim + 1)
-    outwidth = np.int32(inwidth - kdim + 1)
-    
-    layer_name = f'in_{batchsize}X{inchannels}X{indepth}X{inheight}X{inwidth}_k_{kdim}X{kdim}X{kdim}_och_{outchannels}'
-    print(f'INFO: NCDHW layout {layer_name}') 
-    layer_names.append(layer_name)
+        inchannels = np.int32(inchannels)
+        indepth = np.int32(indepth)
+        inheight = np.int32(inheight)
+        inwidth = np.int32(inwidth)
+        outchannels = np.int32(outchannels)
+        batchsize = np.int32(batchsize)
+        outdepth = np.int32(indepth - kdim + 1)
+        outheight = np.int32(inheight - kdim + 1)
+        outwidth = np.int32(inwidth - kdim + 1)
+        
+        layer_name = f'in_{batchsize}X{inchannels}X{indepth}X{inheight}X{inwidth}_k_{kdim}X{kdim}X{kdim}_och_{outchannels}'
+        print(f'INFO: NCDHW layout {layer_name}') 
+        layer_names.append(layer_name)
 
-    cudnn_input, cudnn_kernel, cudnn_output, in_desc, in_data, in_data_g, out_desc, out_data, out_data_g, outdims,  filt_desc, filt_data, filt_data_g, ws_ptr, ws_data, ws_size = cudnnsetlayerdesc(cudnn_context, outdimsinit, conv_desc, convolution_algo, d_input,  d_kernel, d_output, batchsize, kdim, inchannels, indepth, inheight, inwidth, outchannels, data_type, tensor_dim, tensor_format)
+        cudnn_input, cudnn_kernel, cudnn_output, in_desc, in_data, in_data_g, out_desc, out_data, out_data_g, outdims,  filt_desc, filt_data, filt_data_g, ws_ptr, ws_data, ws_size = cudnnsetlayerdesc(cudnn_context, outdimsinit, conv_desc, convolution_algo, d_input,  d_kernel, d_output, batchsize, kdim, inchannels, indepth, inheight, inwidth, outchannels, data_type, tensor_dim, tensor_format)
 
-    # Code for verification
-    if verify:
-        print("INFO: Running verification to compare against cudnn output")
-        run_cudnn()
-        run_optim_dace()
-        d_output = d_output.cpu()
-        dace_output_g = gpuarray.to_gpu(d_output.numpy().astype(np.float32))
-                              
-        diff = np.linalg.norm((out_data_g - dace_output_g).get()) / (batchsize * outchannels * outdepth * outheight * outwidth )
-        print('Difference between cudnn and dace values:', diff)
+        # Code for verification
+        if verify:
+            print("INFO: Running verification to compare against cudnn output")
+            run_cudnn()
+            run_optim_dace()
+            d_output = d_output.cpu()
+            dace_output_g = gpuarray.to_gpu(d_output.numpy().astype(np.float32))
+                                
+            diff = np.linalg.norm((out_data_g - dace_output_g).get()) / (batchsize * outchannels * outdepth * outheight * outwidth )
+            print('Difference between cudnn and dace values:', diff)
 
-        if(diff<=1e-4): #TODO: Check if the threshold should be reduced
-            print(f"Verification successfull")
-        else:
-            sys.exit("!!! ERROR: Incorrect verification")
+            if(diff<=1e-4): #TODO: Check if the threshold should be reduced
+                print(f"Verification successfull")
+            else:
+                sys.exit("!!! ERROR: Incorrect verification")
 
-    # Profiling pytorch using run
-    if runtorch:
-        run_fun(run_torch, warmupiter, totaliter)
+        # Profiling pytorch using run
+        if runtorch:
+            run_fun(run_torch, warmupiter, totaliter)
 
-    # Profiling optimized dace using run
-    if runoptimdace:
-        run_fun(run_optim_dace, warmupiter, totaliter)
+        # Profiling optimized dace using run
+        if runoptimdace:
+            run_fun(run_optim_dace, warmupiter, totaliter)
 
-    # Profiling optimized dace using run
-    if runcudnn:
-        run_fun(run_cudnn, warmupiter, totaliter)
+        # Profiling optimized dace using run
+        if runcudnn:
+            run_fun(run_cudnn, warmupiter, totaliter)
 
-    # Comparitive profiling using time funcs
-    print(f"INFO: Warmup for {warmupiter} iterations and total iterations {totaliter}")
-    print(f"INFO: Statistics for layer number {layern}")
-    dace_median = []
-    torch_median = []
-    cudnn_median = []
-    if compareprof:
-        times = time_funcs([run_optim_dace, run_cudnn],
-                        func_names=["dace", "cudnn"],
-                        warmups=warmupiter,
-                        num_iters=totaliter,
-                        launch_wait=setlaunchwait)
-        print_time_statistics(times, [ "dace", "cudnn"])
-        layersummary['layer_name'] = layer_name
-        layersummary['times'] = times
-        layersummary['dace_median'] = statistics.median(times[0])
-        layersummary['cudnn_median'] = statistics.median(times[1])
-        median_dace.append(statistics.median(times[0]))
-        median_cudnn.append(statistics.median(times[1]))
-        dace_median.append(times[0])
-        cudnn_median.append(times[1])
-        summary.append(layersummary)
+        # Comparitive profiling using time funcs
+        print(f"INFO: Warmup for {warmupiter} iterations and total iterations {totaliter}")
+        print(f"INFO: Statistics for layer number {layern} with batch size of {batchsize}")
+        dace_median = []
+        torch_median = []
+        cudnn_median = []
+        if compareprof:
+            times = time_funcs([run_optim_dace, run_cudnn],
+                            func_names=["dace", "cudnn"],
+                            warmups=warmupiter,
+                            num_iters=totaliter,
+                            launch_wait=setlaunchwait)
+            print_time_statistics(times, [ "dace", "cudnn"])
+            layersummary['layer_name'] = layer_name
+            layersummary['times'] = times
+            layersummary['dace_median'] = statistics.median(times[0])
+            layersummary['cudnn_median'] = statistics.median(times[1])
+            median_dace.append(statistics.median(times[0]))
+            median_cudnn.append(statistics.median(times[1]))
+            dace_median.append(times[0])
+            cudnn_median.append(times[1])
+            summary.append(layersummary)
 
-    dace_median_array = np.array(dace_median)
-    cudnn_median_array = np.array(cudnn_median)
+        dace_median_array = np.array(dace_median)
+        cudnn_median_array = np.array(cudnn_median)
 
-    # run using prof for torch
-    if proftorch:
-        times = time_funcs([run_torch],
-                        func_names=["pytorch"],
-                        warmups=warmupiter,
-                        num_iters=totaliter,
-                        launch_wait=setlaunchwait)
-        print_time_statistics(times, [ "pytorch"])
+        # run using prof for torch
+        if proftorch:
+            times = time_funcs([run_torch],
+                            func_names=["pytorch"],
+                            warmups=warmupiter,
+                            num_iters=totaliter,
+                            launch_wait=setlaunchwait)
+            print_time_statistics(times, [ "pytorch"])
 
-    # run using prof for dace
-    if profoptimdace:
-        times = time_funcs([run_optim_dace],
-                        func_names=["optimdace"],
-                        warmups=warmupiter,
-                        num_iters=totaliter,
-                        launch_wait=setlaunchwait)
-        print_time_statistics(times, [ "optimdace"])
-    
-    if profcudnn:
-        times = time_funcs([run_cudnn],
-                        func_names=["cudnn"],
-                        warmups=warmupiter,
-                        num_iters=totaliter,
-                        launch_wait=setlaunchwait)
-        print_time_statistics(times, [ "cudnn"])
-    
-    in_desc, out_desc, filt_desc, ws_ptr = destroydescinoutfilt(in_desc, out_desc, filt_desc, ws_ptr)
+        # run using prof for dace
+        if profoptimdace:
+            times = time_funcs([run_optim_dace],
+                            func_names=["optimdace"],
+                            warmups=warmupiter,
+                            num_iters=totaliter,
+                            launch_wait=setlaunchwait)
+            print_time_statistics(times, [ "optimdace"])
+        
+        if profcudnn:
+            times = time_funcs([run_cudnn],
+                            func_names=["cudnn"],
+                            warmups=warmupiter,
+                            num_iters=totaliter,
+                            launch_wait=setlaunchwait)
+            print_time_statistics(times, [ "cudnn"])
+        
+        in_desc, out_desc, filt_desc, ws_ptr = destroydescinoutfilt(in_desc, out_desc, filt_desc, ws_ptr)
 
 
 createsummaryfile(summary, outdir, csv_columns)