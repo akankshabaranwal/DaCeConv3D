30beb3ae8843047874b2040af67e29dd9feeb51b
Akanksha Baranwal <abaranwa@ault.cscs.ch>
2023-01-31 15:50:04 +0100

    roofline plot

--- samples/simple/plotRoofline.py
+++ samples/simple/plotRoofline.py
@@ -8,63 +8,71 @@ plt.xlabel("Arithmetic Intensity: FLOPs/Byte")
 plt.ylabel("Performance: GFLOPs/s")
 
 # Peak values picked from here: https://crd.lbl.gov/assets/Uploads/cug19-roofline-final.pdf 
-plt.title("Roofline Log Log Plot for cutlass on GV100 for different cosmoflow layers")
+plt.title("Roofline Log Log Plot for cudnn on GV100 for different cosmoflow layers Batch size 16")
 plt.suptitle("Device: Tesla V100")
 x = np.linspace(0.01, 100000000, 10)
 
 # Max performance metrics
-fp32Max = 14800
-tensorPerf = 118500
-L2roofline = 4198.4 # GB/s
+fp32Max = 12819 # Value updated using nsight compute
+#tensorPerf = 118500
+L2roofline = 4199.4 # GB/s
 DRAMroofline = 900 # GB/s
 
 # Plotting the maximum floating point performance
-plt.axhline(y=fp32Max, color='m', linestyle='--', label='FP32 Maximum 14.8 TFLOPS')
+plt.axhline(y=fp32Max, color='r', linestyle='--')
+plt.text( 10000, fp32Max*0.6, 'FP32 Maximum 12.8 TFLOPS', color='r')
 
-# Plotting tensor core performance
-plt.axhline(y=tensorPerf, linestyle='--', label='Tensor Core maximum 118.5 TFLOPS')
+# # Plotting tensor core performance
+# plt.axhline(y=tensorPerf, linestyle='--', label='Tensor Core maximum 118.5 TFLOPS')
 
 # L2 roofline
 y = L2roofline*x
-plt.loglog(x, y, '-.g', label='L2 4198.4 GB/s')
+plt.loglog(x, y, '--g')
 plt.ylim(100,1e6*0.3)
+plt.text( 0.1, 200, 'L2 4198.4 GB/s', color='g')
 
 # DRAM roofline
 y = DRAMroofline*x
-plt.loglog(x,y,'-.b',label='DRAM 900 GB/s')
+plt.loglog(x, y, '--b')
 plt.ylim(100,1e6*0.3)
+plt.text( 1, 400, 'DRAM 900 GB/s', color='b')
 
+# https://matplotlib.org/stable/api/markers_api.html
 
-# Function points
-layer2_AI = 27
-layer2_Perf = 10001.6
-plt.loglog(layer2_AI, layer2_Perf,'*r')
-plt.text(layer2_AI, layer2_Perf*0.8, 'L2')
+layer0_AI = 15.74
+layer0_Perf = 5201
+L0 = plt.loglog(layer0_AI, layer0_Perf,'o', label='implicit_convolveNd_sgemm')
+plt.annotate('0', (layer0_AI, layer0_Perf))
+ 
+layer1_AI = 28.23
+layer1_Perf = 9655
+L1 = plt.loglog(layer1_AI, layer1_Perf,'v', label='volta_scudnn_128X32_3dconv_fprop_small_nn_v1')
+plt.annotate('1', (layer1_AI, layer1_Perf))
 
-layer3_AI = 55
-layer3_Perf = 32610
-plt.loglog(layer3_AI, layer3_Perf,'*r')
-plt.text(layer3_AI, layer3_Perf*0.8, 'L3')
+layer2_AI = 55.61
+layer2_Perf = 11591
+L2 = plt.loglog(layer2_AI, layer2_Perf,'^', label='volta_scudnn_128X64_3dconv_fprop_small_nn_v1')
+plt.annotate('2', (layer2_AI, layer2_Perf))
 
-layer4_AI = 111
-layer4_Perf = 51796.3
-plt.loglog(layer4_AI, layer4_Perf,'*r')
-plt.text(layer4_AI*0.8, layer4_Perf*0.8, 'L4')
+layer3_AI = 108.85
+layer3_Perf = 11746
+L3 = plt.loglog(layer3_AI, layer3_Perf,'D', label='volta_scudnn_128X128_3dconv_fprop_small_nn_v1')
+plt.annotate('3', (layer3_AI, layer3_Perf))
 
-layer5_AI = 217
-layer5_Perf = 53627.8
-plt.loglog(layer5_AI, layer5_Perf,'*r')
-plt.text(layer5_AI*0.9, layer5_Perf, 'L5')
+layer4_AI = 191.7
+layer4_Perf = 11877
+L4 = plt.loglog(layer4_AI, layer4_Perf,'>', label='volta_scudnn_128X128_3dconv_fprop_small_nn_v1')
+plt.annotate('4', (layer4_AI, layer4_Perf))
 
-layer6_AI = 193
-layer6_Perf = 6799.84
-plt.loglog(layer6_AI, layer6_Perf,'*r')
-plt.text(layer6_AI, layer6_Perf*0.8, 'L6')
+layer5_AI = 247.96
+layer5_Perf = 6857
+L5 = plt.loglog(layer5_AI, layer5_Perf,'<', label='volta_scudnn_128X64_3dconv_fprop_small_nn_v1')
+plt.annotate('5', (layer5_AI, layer5_Perf))
 
-layer7_AI = 83
-layer7_Perf = 877.18
-plt.loglog(layer7_AI, layer7_Perf,'*r')
-plt.text(layer7_AI, layer7_Perf*0.8, 'L7')
+layer6_AI = 54.85
+layer6_Perf = 1439
+L6 = plt.loglog(layer6_AI, layer6_Perf,'s', label='volta_scudnn_128X32_3dconv_fprop_small_nn_v1')
+plt.annotate('6', (layer6_AI, layer6_Perf))
 
 plt.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
 plt.savefig('outputplots/rooflinetest.png', bbox_inches='tight')
